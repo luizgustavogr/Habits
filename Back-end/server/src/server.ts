0f18ddd29916c853b8a13fc34d3dc38bee9cbffa
unknown <gluiz3500@gmail.com>
2023-02-02 18:23:05 -0300

    isolando arquivos do back-end

--- Back-end/server/src/server.ts
+++ Back-end/server/src/server.ts
@@ -1,25 +1,23 @@
 // back-end API RESTful
 
-import Fastify from "fastify"
+import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import {PrismaClient} from '@prisma/client'
+import { prisma } from "./lib/prisma"
+import { appRoutes } from './routes'
+
 
 const app= Fastify()
-const prisma = new PrismaClient()
+
 
 app.register(cors)
-
+app.register(appRoutes)
 /*
  *Método HTTP: get(buscar infos), post(criar algo), put(atualizar algum
     recurso por completo), patch(atualizar algo específico de um recurso), delete(
     deletar um recurso)
 */
 
-app.get('/', async ()=>{
-	const habits= await prisma.habit.findMany()
 
-  return habits
-})
 
 app.listen({ //fazer com que app "ouça" a porta 33333
     port:3333,
